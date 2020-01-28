71f76802b1ce12c26eb0b506eb031c18e6f99bcf
Megyesi Albert <44295612+malbertHE@users.noreply.github.com>
2020-01-28 17:03:43 +0100

    Update Program.cs

--- Principle/OCP/Shapes/Program/MAF.EKE.OCP.ShapesDemo2/Program.cs
+++ Principle/OCP/Shapes/Program/MAF.EKE.OCP.ShapesDemo2/Program.cs
@@ -1,4 +1,4 @@
-﻿using MAF.EKE.OCP.Shapes;
+using MAF.EKE.OCP.Shapes;
 using System;
 
 namespace MAF.EKE.OCP.ShapesDemo
@@ -7,32 +7,32 @@ namespace MAF.EKE.OCP.ShapesDemo
 	{
 		static void Main(string[] args)
 		{
-            try
-            {
-                Console.WriteLine("Add meg a négyzet egyik oldalának relatív méretét:");
-                Console.Write("a = ");
-                ushort aOldal;
-                if (!ushort.TryParse(Console.ReadLine(), out aOldal) || aOldal < 0)
-                {
-                    Console.WriteLine("A négyzet oldal méretének csak pozitív szám adható!");
-                    return;
-                }
+			try
+			{
+				Console.WriteLine("Add meg a négyzet egyik oldalának relatív méretét:");
+				Console.Write("a = ");
+				ushort aOldal;
+				if (!ushort.TryParse(Console.ReadLine(), out aOldal) || aOldal < 0)
+				{
+					Console.WriteLine("A négyzet oldal méretének csak pozitív szám adható!");
+					return;
+				}
 
-                Square square = Square.CreateSquare(aOldal);
-                try
-                {
-                    Console.WriteLine($"A négyzetlap területe = {square.AreaCalculation()}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            finally
-            {
-                Console.WriteLine("Kilépés a programból Enter leütésére.");
-                Console.ReadLine();
-            }
+				Square square = Square.CreateSquare(aOldal);
+				try
+				{
+					Console.WriteLine($"A négyzetlap területe = {square.AreaCalculation()}");
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+			}
+			finally
+			{
+				Console.WriteLine("Kilépés a programból Enter leütésére.");
+				Console.ReadLine();
+			}
 		}
 	}
 }
